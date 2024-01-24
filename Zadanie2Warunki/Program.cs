6222ee639fa0b2e41fc5fa974a7df085eb4674dd
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-24 19:32:49 +0100

    Add project files.

---
+++ Zadanie2Warunki/Program.cs
@@ -0,0 +1,19 @@
+﻿int number;
+int result;
+
+
+Console.WriteLine("Please enter a number: ");
+var enteredNumber = Console.ReadLine();
+
+Int32.TryParse(enteredNumber, out number);
+
+result = number % 2;
+
+if(result != 0)
+{
+    Console.WriteLine("The number is not even");
+}
+else
+{
+    Console.WriteLine("The number is even");
+}
